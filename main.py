972a0c999412fe1a4b1ae57554b26e8f69efa0a1
Treasure Mayowa <69307446+Treasure-Mayowa@users.noreply.github.com>
2023-07-17 12:58:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import pd
 import tensorflow as tf
 from tensorflow.keras.models import Sequential 
 from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Activation
@@ -8,15 +9,13 @@ import matplotlib.pyplot as plt
 import glob
 import os
 
-# after you extract the dataset, 
-# put cell_images folder in the working directory
 img_dir="cell_images"  
 img_size=70
 
 def load_img_data(path):
     image_files = glob.glob(os.path.join(path, "Parasitized/*.png")) + \
                   glob.glob(os.path.join(path, "Uninfected/*.png"))
-    X, y = [], []
+    x, y = [], []
     for image_file in image_files:
         # 0 for uninfected and 1 for infected
         label = 0 if "Uninfected" in image_file else 1
@@ -24,6 +23,6 @@ def load_img_data(path):
         img_arr = cv2.imread(image_file, cv2.IMREAD_GRAYSCALE)
         # resize the image to (70x70)
         img_resized = cv2.resize(img_arr, (img_size, img_size))
-        X.append(img_resized)
+        x.append(img_resized)
         y.append(label)
-    return X, y
+    return x, y
