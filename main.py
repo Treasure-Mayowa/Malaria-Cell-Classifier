8dafad5bda9a61410304ab16c34ceb75800d0335
Treasure Mayowa <69307446+Treasure-Mayowa@users.noreply.github.com>
2023-07-17 13:18:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import glob
+import os
 import cv2
 import tensorflow as tf
 from tensorflow.keras.models import Sequential 
@@ -5,8 +7,7 @@ from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Activatio
 from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
-import glob
-import os
+
 
 img_dir="cell_images"  
 img_size=70
@@ -25,3 +26,52 @@ def load_img_data(path):
         x.append(img_resized)
         y.append(label)
     return x, y
+
+
+# load the data
+x, y = load_img_data(img_dir)
+# reshape to (n_samples, 70, 70, 1) (to fit the NN)
+x = np.array(x).reshape(-1, img_size, img_size, 1)
+# scale pixels from the range [0, 255] to [0, 1] 
+# to help the neural network learn much faster
+x = x / 255 
+
+# shuffle & split the dataset
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, stratify=y)
+print("Total training samples:", x_train.shape)
+print("Total validation samples:", x_test.shape[0])
+
+model = Sequential()
+model.add(Conv2D(64, (3, 3), input_shape=x_train.shape[1:]))
+model.add(Activation("relu"))
+model.add(MaxPool2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation("relu"))
+model.add(MaxPool2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation("relu"))
+model.add(MaxPool2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+
+model.add(Dense(64))
+model.add(Activation("relu"))
+
+model.add(Dense(64))
+model.add(Activation("relu"))
+
+model.add(Dense(1))
+model.add(Activation("sigmoid"))
+
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+# train the model with 3 epochs, 64 batch size
+model.fit(x_train, np.array(y_train), batch_size=64, epochs=3, validation_split=0.2)
+# if you already trained the model, uncomment below and comment above
+# so you can only load the previously trained model
+# model.load_weights("malaria-cell-cnn.h5")
+
+loss, accuracy = model.evaluate(X_test, np.array(y_test), verbose=0)
+print(f"Testing on {len(X_test)} images, the results are\n Accuracy: {accuracy} | Loss: {loss}")
